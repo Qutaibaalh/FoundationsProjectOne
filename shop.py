950107bee394494cc751a2400d0b2e4031369863
Qutaiba98 <qutaiba98@Qutaiba98s-MacBook.local>
2018-06-14 22:03:27 +0300

    submission

--- shop.py
+++ shop.py
@@ -1,4 +1,4 @@
-####################### DO NOT MODIFY THIS CODE ########################
+
 menu = {
     "original cupcake": 2,
     "signature cupcake": 2.750,
@@ -11,39 +11,49 @@ original_price = 2
 signature_price = 2.750
 
 ############################# Start Here! ##############################
-cupcake_shop_name = #complete me!
-signature_flavors = #complete me!
+cupcake_shop_name = "cupncakes"
+signature_flavors = ["peanute", "pistachio", "orange"]
 order_list = []
 
 
 def print_menu():
-    """
-    Print the items in the menu dictionary.
-    """
-    # your code goes here!
+    for item in menu:
+        print("- %s (KD %s)" % (item, menu[item]))
+    
+    
 
 
 def print_originals():
-    """
-    Print the original flavor cupcakes.
-    """
     print("Our original flavor cupcakes (KD %s each):" % original_price)
     # your code goes here!
+    for item in original_flavors:
+        print("- \"%s\"" % item)
 
 
 def print_signatures():
-    """
-    Print the signature flavor cupcakes.
-    """
+    
+    
+
     print("Our signature flavor cupcake (KD %s each):" % signature_price)
-    # your code goes here!
+    for item in signature_flavors:
+        print("- \"%s\"" % item)
 
 
 def is_valid_order(order):
-    """
-    Check if an order exists in the shop.
-    """
-    # your code goes here!
+    for item in menu: 
+        if item == order: 
+            return True  
+    for item in original_flavors:
+        elif item == order:
+                return True
+    for item in signature_flavors:
+        elif item == order 
+            return True 
+    else:
+        return False 
+
+
+
 
 
 def get_order():
@@ -57,18 +67,26 @@ def get_order():
 
 
 def accept_credit_card(total):
-    """
+    
     Return whether an order is eligible for credit card payment.
-    """
-    # your code goes here!
+    if total >= 5:
+        return True
+    else:
+        return False
 
 
 def get_total_price(order_list):
-    """
+    
     Calculate and return total price of the order.
-    """
+    
     total = 0
-    # your code goes here!
+    for order in order_list:
+         if order in menu:
+            total += menu[order]
+        elif order in original_flavors:
+            total += original_price
+        elif order in signature_flavors:
+            total += signature_price
 
     return total
 
